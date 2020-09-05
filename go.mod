b061b2ea2bded76a543267702d953c3a7217ad40
cander <cander_liubiao@126.com>
2020-09-05 11:45:25 +0800

    update

---
+++ go.mod
@@ -0,0 +1,9 @@
+module githook
+
+go 1.13
+
+require (
+	github.com/hyahm/goconfig v0.0.0-20200814144712-bd712352a305
+	github.com/hyahm/golog v0.0.0-20200903091725-fb6aa179364d
+	github.com/hyahm/xmux v0.0.0-20200903093009-e56e6c1d0aa9
+)
--- main.go
+++ main.go
@@ -1,63 +1,79 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"path/filepath"
+
+	"github.com/hyahm/goconfig"
+	"github.com/hyahm/golog"
+	"github.com/hyahm/xmux"
 )
 
-type data struct {
-	Dir string `json:"dir"`
-	Command string `json:"cmd"`
-	User string `json:"user"`
+func hook(w http.ResponseWriter, r *http.Request) {
+	golog.Info(r.Method)
+	token := r.Header.Get("X-Gitlab-Token")
+	golog.Info(token)
+	filename := xmux.Var(r)["filename"]
+	b, err := ioutil.ReadFile(filepath.Join(goconfig.ReadString("server.jsondir"), filename))
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	jf := &JsonFile{}
+	err = json.Unmarshal(b, &jf)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	out, err := jf.shell()
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write(out)
+	return
+
 }
 
-func shell(cmd string) ([]byte, error){
-	c := exec.Command(sh, "-c", cmd)
+type JsonFile struct {
+	User    string
+	Command string
+	Dir     string
+	Shell   string
+}
+
+func (jf *JsonFile) shell() ([]byte, error) {
+	c := exec.Command(jf.Shell, "-c", fmt.Sprintf("cd %s && sudo -u %s git %s", jf.Dir, jf.User, jf.Command))
 	return c.CombinedOutput()
 }
 
-func hook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set(header,token)
-	if r.Method == http.MethodPost {
-		c := fmt.Sprintf("cd %s && sudo -u %s git %s", dir, user, cmd)
-		out, _ := shell(c)
-		w.Write(out)
-		return
-	} else if r.Method == http.MethodGet {
-		c := fmt.Sprintf("cd %s && sudo -u %s git %s", dir, user, cmd)
-		out, _ := shell(c)
-
-		w.Write(out)
-		return
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-}
-
-var addr string
-var sh string
-var header string
-var token string
-var user string
-var cmd string
-var dir string
-
-func init() {
-	flag.StringVar(&addr, "l", ":10009", "listen address; like :10009")
-	flag.StringVar(&sh, "s", "/bin/bash", "bash path")
-	flag.StringVar(&header, "h", "X-Gitlab-Token", "header")
-	flag.StringVar(&token, "t", "123456", "token")
-	flag.StringVar(&cmd, "c", "pull", "cmd pull")
-	flag.StringVar(&user, "u", "root", "cmd pull")
-	flag.StringVar(&dir, "d", "/var/www/test", "dir name")
-}
-
 func main() {
-	flag.Parse()
-	http.HandleFunc("/", hook)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	conf := "hook.ini"
+	if len(os.Args) > 1 {
+		conf = os.Args[1]
+	}
+	goconfig.InitConf(conf, goconfig.INI)
+	_, err := os.Stat(goconfig.ReadString("server.jsondir"))
+	if os.IsNotExist(err) {
+		if err = os.MkdirAll(goconfig.ReadString("server.jsondir"), 0755); err != nil {
+			log.Fatal(err)
+		}
+	}
+	router := xmux.NewRouter()
+	router.SetHeader("Access-Control-Allow-Origin", "*")
+	router.SetHeader("Content-Type", "application/x-www-form-urlencoded,application/json; charset=UTF-8")
+	router.SetHeader("Access-Control-Allow-Headers", "Content-Type,Access-Token,X-Token,smail,X-Gitlab-Token")
+	router.Post("/post/{filename}", hook)
+	router.Get("/get/{filename}", hook)
+	golog.Info("listen on ", goconfig.ReadString("server.listen", ":10009"))
+	err = router.Run(goconfig.ReadString("server.listen", ":10009"))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
